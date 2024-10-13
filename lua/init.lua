f77185264247bcd3ddd1f40740e06918356784a0
Frank Jeschke <frank@fjes.de>
2024-10-13 20:30:04 +0200

    Add basic and nice treesitter config

--- lua/init.lua
+++ lua/init.lua
@@ -36,3 +36,21 @@ require("fjes")
 require("fjes.colors")
 
 MyColorScheme("tokyonight-moon")
+
+-- FIXME may put the treesitter config into an after moduel, at the moment
+-- after/plugin is a symlink to old vim configurations
+require'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "bash" },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+
+    highlight = {
+        enable = true,
+    },
+}
