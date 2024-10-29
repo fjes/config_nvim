9aabdcb7f6e4943ec9b637e6237fa78a7e316eef
Frank Jeschke <fjeschke@synapticon.com>
2024-10-29 13:46:07 +0100

    Add Telescope Copilot plugin

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    tag = '0.1.8',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    dependencies = {
+        -- "nvim-lua/popup.nvim",
+        "nvim-lua/plenary.nvim",
+    },
+}
