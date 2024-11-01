eae89a960f5407a781a177cda7755b5564d59f25
Frank Jeschke <frank@fjes.de>
2024-11-01 16:09:03 +0100

    Update lsp config into it's own file

--- lua/init.lua
+++ lua/init.lua
@@ -1,43 +1,5 @@
 require("lazyinit")
-
--- configure the used language server
-require'lspconfig'.pylsp.setup{}
-
--- configure the clangd
-require 'lspconfig'.clangd.setup{
-    filetypes = { "c", "cpp", "xc", "objc", "objcpp", "cuda", "proto" },
-    root_dir = function(fname)
-        -- Find the roo directoy containing the configure.json file
-        return lspconfig.util.root_pattern("compile_commands.json")(fname) or
-            lspconfig.util.find_git_ancestor(fname) or
-            vim.fn.getcwd()
-    end,
-}
-
--- Add lua-language-server
---require 'lspconfig'.lua_ls.setup{}
-
--- Add SQL language server
--- This is directly fetched form the README at https://github.com/sqls-server/sqls
--- It is not easy to figure out what the configuration shall look like.
-require'lspconfig'.sqls.setup{
---[[
-	on_attach = function(client, bufnr)
-		require('sqls').on_attach(client, bufnr)
-	end
-	settings = {
-		sqls = {
-			connections = {
-				{
-					driver = 'sqlite3',
-					dataSourceName = '',
-				},
-			},
-		},
-	},
---]]
-}
-
+require("lsp_configs")
 require("fjes")
 
 MyColorScheme("tokyonight-moon")
---
+++ lua/lsp_configs.lua
@@ -0,0 +1,42 @@
+-- Configurations for my language servers.
+-- FIXME maybe put these in individaul files inside the fjes or plugin fodler?
+-- Or even the after folder?
+
+-- configure the used language server
+require'lspconfig'.pylsp.setup{}
+
+-- configure the clangd
+require 'lspconfig'.clangd.setup{
+    filetypes = { "c", "cpp", "xc", "objc", "objcpp", "cuda", "proto" },
+    root_dir = function(fname)
+        -- Find the roo directoy containing the configure.json file
+        return lspconfig.util.root_pattern("compile_commands.json")(fname) or
+            lspconfig.util.find_git_ancestor(fname) or
+            vim.fn.getcwd()
+    end,
+}
+
+-- Add lua-language-server
+--require 'lspconfig'.lua_ls.setup{}
+
+-- Add SQL language server
+-- This is directly fetched form the README at https://github.com/sqls-server/sqls
+-- It is not easy to figure out what the configuration shall look like.
+require'lspconfig'.sqls.setup{
+--[[
+	on_attach = function(client, bufnr)
+		require('sqls').on_attach(client, bufnr)
+	end
+	settings = {
+		sqls = {
+			connections = {
+				{
+					driver = 'sqlite3',
+					dataSourceName = '',
+				},
+			},
+		},
+	},
+--]]
+}
+
