fd3208cb4549c2c0a9c3a754d92692bbf5537071
Frank Jeschke <frank@fjes.de>
2024-11-17 08:13:10 +0100

    Remove duplicate entry

--- after/plugin/lspkeymaps.lua
+++ after/plugin/lspkeymaps.lua
@@ -14,6 +14,5 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
         vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
         vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
     end,
 })
