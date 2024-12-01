8ab670105232ca43231aa377fc99606cfd94b897
Frank Jeschke <frank@fjes.de>
2024-12-01 08:51:44 +0100

    Add Iron plugin to call REPLs
    Iron can be used for nay REPL, it is basically the configured REPL for
    the buffer loaded in a terminal window.

---
+++ after/plugin/iron.lua
@@ -0,0 +1,29 @@
+local iron_config = {
+    repl_open_cmd = 'belowright 10 split',
+    config = {
+        -- Define your REPL commands here
+        repl_definition = {
+            python = {
+                command = {"ipython", "--no-autoindent"},
+            },
+            -- Example for Zig, if you want to add it
+            zig = {
+                command = {"zig", "run"},
+            },
+        },
+        -- Define keymaps for sending code
+        keymaps = {
+            send_motion = "<space>sc",
+            visual_send = "<space>sc",
+            send_file = "<space>sf",
+            send_line = "<space>sl",
+            send_until_cursor = "<space>su",
+            send_mark = "<space>sm",
+            mark_motion = "<space>mc",
+            mark_visual = "<space>mc",
+            remove_mark = "<space>md",
+        },
+    }
+}
+
+require("iron.core").setup(iron_config)
---
+++ lua/plugins/iron.lua
@@ -0,0 +1,3 @@
+return {
+    "Vigemus/iron.nvim",
+}
