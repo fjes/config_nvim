ae9f316d69fdd8a67ed5c898b508228c942376f2
Frank Jeschke <frank@fjes.de>
2025-09-21 09:43:16 +0200

    Change all configs to the new vim.lsp.config() sheme
    Everything seem to work except for the clangd plugin seem to have
    some issues I cannot yet see what may causes it.
    
    These changes are necessary since the neovim v0.11.4 update

--- lua/lsp_configs.lua
+++ lua/lsp_configs.lua
@@ -5,21 +5,35 @@
 local lspconfig = require("lspconfig")
 
 -- configure the used language server
-require'lspconfig'.pylsp.setup{}
+--require'lspconfig'.pylsp.setup{}
+
+-- Apparently this is the way to use now, but it doesn't work as expected. There is something I do not yet understand!
+vim.lsp.config('pylsp', {
+    filetypes = { "py" },
+})
+vim.lsp.enable('pylsp')
 
 -- configure the clangd
-require'lspconfig'.clangd.setup{
+-- new: the new lsp config dos not support the root pattern thing to find the
+-- `compile_commands.json` file which is used by the clangd language server.
+-- But somehow it looks like this LSP is not really working as expected for me.
+-- Even the default LSP keybindings seem to be off.
+vim.lsp.config('clangd', {
     filetypes = { "c", "cpp", "xc", "objc", "objcpp", "cuda", "proto", },
+--[[
     root_dir = function(fname)
         -- Find the roo directoy containing the configure.json file
         return lspconfig.util.root_pattern("compile_commands.json")(fname) or
             lspconfig.util.find_git_ancestor(fname) or
             vim.fn.getcwd()
     end,
-}
+--]]
+})
+vim.lsp.enable('clangd')
 
 -- Add lua-language-server
-require('lspconfig').lua_ls.setup{
+--require('lspconfig').lua_ls.setup{
+vim.lsp.config('lua_ls', {
     settings = {
         Lua = {
             runtime = {
@@ -34,12 +48,14 @@ require('lspconfig').lua_ls.setup{
             },
         }
     }
-}
+})
+vim.lsp.enable('lua_ls')
 
 -- Add SQL language server
 -- This is directly fetched form the README at https://github.com/sqls-server/sqls
 -- It is not easy to figure out what the configuration shall look like.
-require'lspconfig'.sqls.setup{
+--require'lspconfig'.sqls.setup{
+vim.lsp.config('spls', {
 --[[
 	on_attach = function(client, bufnr)
 		require('sqls').on_attach(client, bufnr)
@@ -55,5 +71,6 @@ require'lspconfig'.sqls.setup{
 		},
 	},
 --]]
-}
+})
+vim.lsp.enable('spls')
 
